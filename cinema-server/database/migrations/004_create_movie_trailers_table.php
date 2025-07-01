822a9889c0238316fc83f2a48ace017c3c3a4780
HusseinM20 <130611739+HusseinM20@users.noreply.github.com>
2025-07-02 02:17:21 +0300

    Create movie trailers

---
+++ cinema-server/database/migrations/004_create_movie_trailers_table.php
@@ -0,0 +1,16 @@
+<?php
+require_once __DIR__ . '/../../connection/db.php';
+
+$sql = "CREATE TABLE IF NOT EXISTS movie_trailers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    movie_id INT NOT NULL,
+    trailer_url VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    FOREIGN KEY (movie_id) REFERENCES movies(id) ON DELETE CASCADE
+) ENGINE=InnoDB;";
+
+if ($conn->query($sql) !== TRUE) {
+    die("Error creating table 'movie_trailers': " . $conn->error);
+}
+
+?>
